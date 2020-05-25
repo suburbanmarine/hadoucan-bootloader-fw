35c69b49cfdef172e120e79df4d526f3752e156c
Jacob Schloss <jacob@schloss.io>
2020-05-25 12:08:22 -0700

    log ver info

--- src/sw_ver.cpp
+++ src/sw_ver.cpp
@@ -1,6 +1,6 @@
 #include "sw_ver.hpp"
 
-extern const char* GIT_COMMIT;
-extern const int SW_VER_MAJOR;
-extern const int SW_VER_MINOR;
-extern const int SW_VER_PATCH;
+const char GIT_COMMIT[] = GIT_SHA1;
+const int SW_VER_MAJOR = 0;
+const int SW_VER_MINOR = 1;
+const int SW_VER_PATCH = 0;
