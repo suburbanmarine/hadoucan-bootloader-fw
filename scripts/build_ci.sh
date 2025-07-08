1bde5c276eb2c8a10f4fc15d58e8b5bb6140020f
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2025-07-07 23:38:00 -0700

    reduce nesting

--- scripts/build_ci.sh
+++ scripts/build_ci.sh
@@ -13,9 +13,9 @@ docker stop $CONTAINER_ID
 
 pushd $GITHUB_WORKSPACE/build/flash/debug/
 sha256sum -b stm32h7_qspi_boot.elf stm32h7_qspi_boot.bin | tee sha256.txt
-tar -czf $GITHUB_WORKSPACE/hadoucan-bootloader-fw-debug-flash-$GITHUB_SHA.tar.gz stm32h7_qspi_boot.elf stm32h7_qspi_boot.bin sha256.txt
+tar -czf $GITHUB_WORKSPACE/hadoucan-bootloader-fw-debug-$GITHUB_SHA.tar.gz stm32h7_qspi_boot.elf stm32h7_qspi_boot.bin sha256.txt
 
 pushd $GITHUB_WORKSPACE/build/flash/release/
 sha256sum -b stm32h7_qspi_boot.elf stm32h7_qspi_boot.bin | tee sha256.txt
-tar -czf $GITHUB_WORKSPACE/hadoucan-bootloader-fw-release-flash-$GITHUB_SHA.tar.gz stm32h7_qspi_boot.elf stm32h7_qspi_boot.bin sha256.txt
+tar -czf $GITHUB_WORKSPACE/hadoucan-bootloader-fw-release-$GITHUB_SHA.tar.gz stm32h7_qspi_boot.elf stm32h7_qspi_boot.bin sha256.txt
 popd
