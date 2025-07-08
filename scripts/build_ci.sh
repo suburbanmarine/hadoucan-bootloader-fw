8638cd1a9cc7f7b8db616a4496335fdbaef019b8
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2025-07-07 21:15:30 -0700

    add debug

--- scripts/build_ci.sh
+++ scripts/build_ci.sh
@@ -11,10 +11,11 @@ docker exec -u $(id -u):$(id -g) -w /tmp/workspace/ $CONTAINER_ID make -j`nproc`
 docker exec -u $(id -u):$(id -g) -w /tmp/workspace/ $CONTAINER_ID make -j`nproc` -C build/flash/release/
 docker stop $CONTAINER_ID
 
-pushd $GITHUB_WORKSPACE/build/ram/debug/
-ls -la
+pushd $GITHUB_WORKSPACE/build/flash/debug/
+sha256sum -b stm32h7_qspi_boot.elf stm32h7_qspi_boot.bin | tee sha256.txt
+tar -czf $GITHUB_WORKSPACE/stm32h7_qspi_boot-debug-flash-$GITHUB_SHA.tar.gz stm32h7_qspi_boot.elf stm32h7_qspi_boot.bin sha256.txt
 
 pushd $GITHUB_WORKSPACE/build/flash/release/
 sha256sum -b stm32h7_qspi_boot.elf stm32h7_qspi_boot.bin | tee sha256.txt
-tar -czf $GITHUB_WORKSPACE/stm32h7_qspi_boot-release-flash-$GITHUB_SHA.tar.gz  stm32h7_qspi_boot.elf stm32h7_qspi_boot.bin sha256.txt
+tar -czf $GITHUB_WORKSPACE/stm32h7_qspi_boot-release-flash-$GITHUB_SHA.tar.gz stm32h7_qspi_boot.elf stm32h7_qspi_boot.bin sha256.txt
 popd
